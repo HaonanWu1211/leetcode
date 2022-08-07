2c4a2c54da702263a82bae2e207f77464839f999
haonan <whn1211@126.com>
2022-08-07 15:22:45 +0800

    04

---
+++ 剑指offer-04.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool findNumberIn2DArray(vector<vector<int>>& matrix, int target) {
+        //二叉搜索树的思想
+        if(matrix.size() <= 0 || matrix[0].size() <= 0)
+            return false;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int i = m-1;
+        int j = 0;
+        while(i >= 0 && j < n){
+            if(matrix[i][j] == target)
+                return true;
+            else if(matrix[i][j] > target)
+                i--;
+            else
+                j++;
+        }
+        return false;
+    }
+};
