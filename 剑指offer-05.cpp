d7ee0a652c2296d4e37ca2d386eb9a69002acc71
haonanwu <haonanwu@amd.com>
2022-08-08 17:43:45 +0800

    05

---
+++ 剑指offer-05.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    string replaceSpace(string s) {
+        string res = "";
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] != ' '){
+                res += s[i];
+            }else{
+                res += "%20";
+            }
+        }
+        return res;
+    }
+};
