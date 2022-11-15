056b51987c3a2e7f1a0f871e1cb096528ecb3724
haonan <whn1211@126.com>
2022-11-15 15:41:23 +0800

    leetcode-1710

---
+++ leetcode-1710.cpp
@@ -0,0 +1,18 @@
+// Ì°ÐÄ
+class Solution {
+public:
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        sort(boxTypes.begin(), boxTypes.end(), [](vector<int> a, vector<int> b){
+            return a[1] > b[1];
+        });
+        int i = 0;
+        int n = boxTypes.size();
+        int res = 0;
+        while(truckSize > 0 && i < n){
+            res += min(truckSize, boxTypes[i][0])*boxTypes[i][1];
+            truckSize -= min(truckSize, boxTypes[i][0]);
+            i++;
+        }
+        return res;
+    }
+};
